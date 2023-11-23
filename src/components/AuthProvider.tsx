ae028bc3e77e430d33169f9348c6e5d4c088bb08
sagartechsoln <skshadab1234@gmail.com>
2023-11-23 19:19:31 +0530

    Updated Uiux

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -10,14 +10,7 @@ export default function Provider({
     children: React.ReactNode;
     session: any;
 }): React.ReactNode {
-    // Check if there is no session and if the user is trying to access an excluded route
-    const isExcludedRoute = excludedRoutes.some(route => window.location.pathname.startsWith(route));
 
-    if (!session && typeof window !== 'undefined' && isExcludedRoute) {
-        // Redirect to the home page
-        window.location.href = '/';
-        return null; // Return null to prevent rendering the rest of the components
-    }
 
     return <SessionProvider session={session}>{children}</SessionProvider>;
 }
